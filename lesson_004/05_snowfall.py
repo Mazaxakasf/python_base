91e576f8a1ca07be8d108837e396e239f994aee6
Mazaxakasf <106738591+Mazaxakasf@users.noreply.github.com>
2022-08-02 23:47:11 +0300

    not completed

---
+++ lesson_004/05_snowfall.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+import simple_draw as sd
+
+sd.resolution = (1200, 600)
+# На основе кода из практической части реализовать снегопад:
+# - создать списки данных для отрисовки N снежинок
+# - нарисовать падение этих N снежинок
+# - создать список рандомных длинн лучей снежинок (от 10 до 100) и пусть все снежинки будут разные
+
+N = 20
+
+# Пригодятся функции
+# sd.get_point()
+# sd.snowflake()
+# sd.sleep()
+# sd.random_number()
+# sd.user_want_exit()
+
+
+def snowflakes(length):
+    random_x = sd.random_number(0, 1000)
+    y = 50
+    while True:
+        sd.clear_screen()
+
+        point = sd.get_point(random_x, y)
+        sd.snowflake(center=point, length=length)
+
+        y -= 10
+
+
+        if y < 50:
+            break
+
+        sd.sleep(0.1)
+        if sd.user_want_exit():
+            break
+
+
+snowflakes(length=40)
+
+
+
+
+
+sd.pause()
+
+
+
+
+# подсказка! для ускорения отрисовки можно
+#  - убрать clear_screen()
+#  - в начале рисования всех снежинок вызвать sd.start_drawing()
+#  - на старом месте снежинки отрисовать её же, но цветом sd.background_color
+#  - сдвинуть снежинку
+#  - отрисовать её цветом sd.COLOR_WHITE на новом месте
+#  - после отрисовки всех снежинок, перед sleep(), вызвать sd.finish_drawing()
+
+
+# 4) Усложненное задание (делать по желанию)
+# - сделать рандомные отклонения вправо/влево при каждом шаге
+# - сделать сугоб внизу экрана - если снежинка долетает до низа, оставлять её там,
+#   и добавлять новую снежинку
+# Результат решения см https://youtu.be/XBx0JtxHiLg
+
+
